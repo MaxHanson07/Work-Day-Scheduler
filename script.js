440cebf82d3552efb29677c86986110be294e2d0
Max Hanson <mthanson2@gmail.com>
2020-09-18 20:20:54 -0700

    Fixed some css problems

--- script.js
+++ script.js
@@ -19,20 +19,16 @@ console.log(moment().format("h A"));
 var newRow = $("<div>");
 newRow.attr("class", "row", "time-block")
 
-// TODO: will be used in loop later when creating rows
-
-
-// TODO: SHow the time on top of the calender, Using moment.js grab the time, and set the text of the P tag with the id of currentDay to be our time from moment.js
-// Variable to be used to dispaly on html and later for if statements determining if events have past
+// Display time on top of the calender, Using moment.js grab the time
+// Set the text of the P tag with the id of currentDay to be our time from moment.js
 var currentTimeDisplay = (moment().format('MMMM Do YYYY, h:mm:ss a'));
 $("#currentDay").text(currentTimeDisplay);
 
-// TODO:  Create an array to hold the hours that I need ["9 AM", "10 AM", "..." "12 PM" etc]
+// Create an array to hold the hours that I need
 var hours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
 var hr = 8;
 
-// TODO:  using a for loop
-// TODO: loop over my array of hours and create a div for row, create div for hours, textarea, and a button\
+// Loop over my array of hours and create a div for row, create div for hours, textarea, and a button
 for (let i = 0; i < hours.length; i++) {
     // Creates the three columns
     // Contains the hour
@@ -55,7 +51,7 @@ for (let i = 0; i < hours.length; i++) {
 
     // Adds saved content to text area it belongs to
     var textFromLocalStorage = localStorage.getItem(saveCol.attr("data-hour"))
-    $(".first").text(text1FromLocalStorage)
+    textCol.text(textFromLocalStorage);
 
     // Compare the current hour to each hour on webpage (To learn which ones have past)
     // Give the elements (hour block) the classes that they need based on if they are past, present, or future
@@ -63,26 +59,29 @@ for (let i = 0; i < hours.length; i++) {
     var currentHr = moment().format("H");
     console.log(currentHr);
     console.log(textCol.attr("id"))
-    // TODO: Acting funky
-    // if (currentHr > textCol.attr("id")) {
-    //     textCol.attr("class", "past");
-    // }
-    // else if (currentHr === textCol.attr("id")) {
-    //     textCol.attr("class", "present");
-    // }
-    // else {
-    //     textCol.attr("class", "future");
-    // }
+    console.log(currentHr > textCol.attr("id"))
 
-    // TODO: Create click event listener for my buttons 
+    // Sets the hour blocks to their corresponding class in order to color code them in CSS
+    if (currentHr > textCol.attr("id")) {
+        textCol.addClass("past");
+    }
+    else if (currentHr === textCol.attr("id")) {
+        textCol.addClass("present");
+    }
+    else {
+        textCol.addClass("future");
+    }
+
+    // TODO: Create click event listener for my save buttons 
     saveCol.on("click", function (event) {
         // Grab the value of the text area and save it to a var in order to store it in local storage later
         var enteredText = textCol.val();
-        console.log(enteredEvent);
+        console.log(enteredText);
 
         // TODO: using localStorage.setItem save the text to local storage
-        localStorage.setItem(this.attr("data-hour"), enteredText)
-        localStorage.setItem("hour", hr)
+        // localStorage.setItem(this.attr("data-hour"), enteredText)
+        localStorage.setItem(saveCol.attr("data-hour"), enteredText)
+        // localStorage.setItem("hour", hr)
 
 
     })
