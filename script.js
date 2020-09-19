76aeb5d7161038ed1f26c8ef162242672be11499
Max Hanson <mthanson2@gmail.com>
2020-09-18 19:03:22 -0700

    Made time blocks responsive to the current time

--- script.js
+++ script.js
@@ -22,67 +22,53 @@ newRow.attr("class", "row", "time-block")
 // TODO: will be used in loop later when creating rows
 
 
-
-
-
 // TODO: SHow the time on top of the calender, Using moment.js grab the time, and set the text of the P tag with the id of currentDay to be our time from moment.js
 // Variable to be used to dispaly on html and later for if statements determining if events have past
 var currentTimeDisplay = (moment().format('MMMM Do YYYY, h:mm:ss a'));
 $("#currentDay").text(currentTimeDisplay);
 
-
-
 // TODO:  Create an array to hold the hours that I need ["9 AM", "10 AM", "..." "12 PM" etc]
-var hours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
+var hours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+var hr = 8;
 
 // TODO:  using a for loop
 // TODO: loop over my array of hours and create a div for row, create div for hours, textarea, and a button\
 for (let i = 0; i < hours.length; i++) {
     // Creates the three columns
     // Contains the hour
+    hr++;
     var hourCol = $("<div>");
     hourCol.attr("class", "col-md-2", "hour")
     hourCol.text(hours[i])
     // Contains the textarea for user's event
     var textCol = $("<textarea>");
-    textCol.attr("class", "col-md-8")
+    textCol.attr("class", "col-md-8");
+    textCol.attr("id", hr);
     // Contains the save button
     var saveCol = $("<button>");
     saveCol.attr("class", "col-md-2", "saveBtn")
-    saveCol.text("Save")
+    saveCol.text("Save");
+    saveCol.attr("data-hour", hr);
     // Appends the three columns to the row
     newRow.append(hourCol, textCol, saveCol);
     $(".container").append(newRow);
 
-   
-    // Create variable that contains the hour in the timeblock in order to compare it to current time
-    var thisHour = moment(hours[i], 'h:mma');
-    var currentTime = moment('h:mma')
-    // console.log(currentTime)
-    // console.log(thisHour._i);
-    
-    console.log(thisHour.isBefore(currentTime))
-    // thisHour = thisHour.toDate();
-    // console.log(thisHour.isBefore(currentTime));
-
-    // if (thisHour.isBefore(currentTime)) {
-        
-    // }
+    // Compare the current hour to each hour on webpage (To learn which ones have past)
+    // Give the elements (hour block) the classes that they need based on if they are past, present, or future
+    // TODO: Fix 9:00 Am
+    var currentHr = moment().format("H");
+    console.log(currentHr);
+    console.log(textCol.attr("id"))
+    if (currentHr > textCol.attr("id")) {
+        textCol.attr("class", "past");
+    }
+    else if (currentHr === textCol.attr("id")) {
+        textCol.attr("class", "present");
+    }
+    else {
+        textCol.attr("class", "future");
+    }
 }
-
-// TODO: give the elements some content 
-
-// TODO: How can I compare the hours of my rows, to the current time? (Use moment.js get the current time and compare to the time that is being assigned to the row)
-
-// TODO:  give the elements the classes that they need e.g using an if statment we can check using monet.js to see if the hour that we're looping ove is past the current hour if so give the text area the class of past
-
-
-
-var beginningTime = moment('8:45AM', 'h:mma');
-var endTime = moment('9:00AM', 'h:mma');
-console.log(beginningTime.isBefore(endTime)); // true
-console.log(beginningTime.toDate()); // Mon May 12 2014 08:45:00
-console.log(endTime.toDate()); // Mon May 12 2014 09:00:00
 
 // TODO: Create click event listener for my buttons 
 
